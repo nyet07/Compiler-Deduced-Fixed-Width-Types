e92903ab1bf425afeff01d632588f8fa6ffca00d
Ugur Buyukdurak <ugurbd@gmail.com>
2017-02-20 10:00:37 +0200

    Add files via upload

---
+++ test/main.cpp
@@ -0,0 +1,9 @@
+#include "fixed_types.hpp"
+#include "system_prop.hpp"
+                                /*      Author : Ugur Buyukdurak      */
+using namespace std;
+
+int main() {
+        display_sizes<uint8, int16, uint32, int64, uint64>();
+	return 0;
+}
